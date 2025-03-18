97e5577aa5d868414273826252406dfbb130275a
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-03-17 22:34:34 -0700

    wip: added boilerplate from mens stack auth template code

--- serve.js
+++ serve.js
@@ -6,11 +6,8 @@ const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
-const isSignedIn = require('./middleware/is-signed-in.js');
-const passUserToView = require('./middleware/pass-user-to-view.js');
 
 const authController = require('./controllers/auth.js');
-const applicationsController = require('./controllers/applications.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
 
@@ -32,10 +29,21 @@ app.use(
 );
 
 app.get('/', (req, res) => {
-      res.render('index.ejs');
+  res.render('index.ejs', {
+    user: req.session.user,
+  });
 });
 
-app.listen(port, () => {
-    console.log(`The express app is ready on port ${port}!`);
+app.get('/vip-lounge', (req, res) => {
+  if (req.session.user) {
+    res.send(`Welcome to the party ${req.session.user.username}.`);
+  } else {
+    res.send('Sorry, no guests allowed.');
+  }
 });
-  
+
+app.use('/auth', authController);
+
+app.listen(port, () => {
+  console.log(`The express app is ready on port ${port}!`);
+});
