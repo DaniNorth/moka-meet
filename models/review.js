e48a354ef98262ac7c84e0ce1b11cc937dbd2b01
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-03-19 10:32:28 -0700

    wip: added reviews model to handle user reviews for each coffee shop

---
+++ models/review.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    coffeeShopId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CoffeeShop",
+      required: true,
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: 1,
+      max: 5,
+    },
+    comment: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Review = mongoose.models.Review || mongoose.model("Review", reviewSchema);
+module.exports = Review;
