ad9d199938de7c0bd3e2f509bdefae2fbcc3017e
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-03-19 10:19:46 -0700

    wip: updated user model to handle sign-in and future info related to coffee shops

--- models/user.js
+++ models/user.js
@@ -1,16 +1,62 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const userSchema = mongoose.Schema({
-  username: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    passwordHash: {
+      type: String,
+      required: true,
+    },
+    bio: {
+      type: String,
+    },
+    profilePic: {
+      type: String,
+    },
+    following: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    followers: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+    lists: {
+      visited: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "CoffeeShop",
+        },
+      ],
+      wishlist: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "CoffeeShop",
+        },
+      ],
+      removed: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "CoffeeShop",
+        },
+      ],
+    },
   },
-  password: {
-    type: String,
-    required: true,
-  },
-});
+  { timestamps: true }
+);
 
-const User = mongoose.model('User', userSchema);
-
+const User = mongoose.models.User || mongoose.model("User", userSchema);
 module.exports = User;
