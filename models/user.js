606ad8baf1347efd469fea4b8f63748e9f46c52f
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-03-19 23:49:32 -0700

    wip: removed email requirement from user model. Too much typing for user, may replace with google sign-on after mvp.

--- models/user.js
+++ models/user.js
@@ -7,12 +7,7 @@ const userSchema = new mongoose.Schema(
       required: true,
       unique: true,
     },
-    email: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    passwordHash: {
+    password: {
       type: String,
       required: true,
     },
