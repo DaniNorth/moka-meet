163ed315420dcd667652712413eb31e298fcbfd1
https://github.com/DaniNorth <danielle.b.north@gmail.com>
2025-03-19 23:39:45 -0700

    wip: added routes for adding new coffee shop and viewing the shop in through link for more details

--- controllers/coffeeShops.js
+++ controllers/coffeeShops.js
@@ -1,28 +1,61 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const CoffeeShop = require('../models/coffeeShop');
+const CoffeeShop = require("../models/coffeeShop");
+const User = require("../models/user");
+const Review = require("../models/review");
 
-// GET coffee shops (all or filtered by user)
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
   try {
     let coffeeShops;
-    
-    if (req.query.filter === 'user' && req.session.user) {
-      // Show only coffee shops added by the signed-in user
+
+    if (req.query.filter === "user" && req.session.user) {
       coffeeShops = await CoffeeShop.find({ addedBy: req.session.user._id });
     } else {
-      // Show all coffee shops
       coffeeShops = await CoffeeShop.find();
     }
 
-    res.render('coffeeShops/index.ejs', {
+    res.render("coffeeShops/index.ejs", {
       coffeeShops: coffeeShops,
       user: req.session.user,
-      filter: req.query.filter || 'all'
+      filter: req.query.filter || "all",
     });
   } catch (error) {
     console.log(error);
-    res.redirect('/');
+    res.redirect("/");
+  }
+});
+
+router.get("/new", async (req, res) => {
+  res.render("coffeeShops/new.ejs");
+});
+
+router.post("/", async (req, res) => {
+  try {
+    if (!req.session.user) {
+      return res.redirect("/auth/sign-in");
+    }
+    req.body.addedBy = req.session.user._id;
+    await CoffeeShop.create(req.body);
+    res.redirect("/coffeeShops");
+  } catch (error) {
+    console.error(error);
+    res.redirect("/");
+  }
+});
+
+router.get("/:coffeeShopId", async (req, res) => {
+  try {
+    const coffeeShop = await CoffeeShop.findById(
+      req.params.coffeeShopId
+    ).populate("addedBy");
+    if (!coffeeShop) {
+      return res.redirect("/coffeeShops");
+    }
+
+    res.render("coffeeShops/show.ejs", { coffeeShop });
+  } catch (error) {
+    console.error(error);
+    res.redirect("/coffeeShops");
   }
 });
 
